09f5a5bc86c60aaa91e180de9e1da57f15d88977
Gonza Martín <gonzaemartin@outlook.com>
2023-03-26 15:42:39 -0300

    "Nueva Experiencia" en funcionamiento

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,7 +7,7 @@ import { HeaderComponent } from './components/header/header.component';
 import { LogoAPComponent } from './components/logo-ap/logo-ap.component';
 import { BannerComponent } from './components/banner/banner.component';
 import { AcercaDeComponent } from './components/acerca-de/acerca-de.component';
-import { ExperienciaComponent } from './components/experiencia/experiencia.component';
+import { ExperienciaComponent } from "./components/experiencia/experiencia.component";
 import { EducacionComponent } from './components/educacion/educacion.component';
 import { NgCircleProgressModule} from 'ng-circle-progress';
 import { HysComponent } from './components/hys/hys.component';
@@ -17,6 +17,7 @@ import { HttpClientModule } from '@angular/common/http';
 import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/login/login.component';
 import { interceptorProvider } from './service/interceptor-service';
+import { NewExperienciaComponent } from './components/experiencia/new-experiencia.component';
 
 @NgModule({
   declarations: [
@@ -32,6 +33,7 @@ import { interceptorProvider } from './service/interceptor-service';
     FooterComponent,
     HomeComponent,
     LoginComponent,
+    NewExperienciaComponent,
   ],
   imports: [
     BrowserModule,
