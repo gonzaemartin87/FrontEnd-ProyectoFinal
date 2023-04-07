84d383addad66ae784d02b1c68ae74780c812010
Gonza Martín <gonzaemartin@outlook.com>
2023-04-07 12:47:39 -0300

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -12,7 +12,7 @@ export const environment = {
   production: false,
 
 //URL: 'http://localhost:8080/',
-URL: 'https://portfoliopersonalbackend.onrender.com'
+URL: 'https://backendfinal.fly.dev/'
  };
 
 
