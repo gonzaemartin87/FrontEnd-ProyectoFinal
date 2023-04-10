443e26b00b7b39594122ac90a93750e700db8818
Gonza Martín <gonzaemartin@outlook.com>
2023-04-10 19:56:53 -0300

    .

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -11,8 +11,8 @@ export const environment = {
   },
   production: false,
 
-URL: 'http://localhost:8080/',
-//URL: 'https://portfoliopersonalbackend.onrender.com/'
+//URL: 'http://localhost:8080/',
+URL: 'https://portfoliopersonalbackend.onrender.com/'
  };
 
 
